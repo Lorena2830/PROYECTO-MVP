d76e2d96fca0328b3d112e2d4a09d705d85d2727
Lorena <lorelore9499@gmail.com>
2024-01-29 16:53:08 +0000

    funciona ok aún sin las colisiones

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("keydown", function (event) {
 function gameStart() {
   var enemyId = setInterval(createEnemy, 6000); 
 }
-function createEnemy () {
+function createEnemy () {     
   var enemy = new Enemy(300, 650, board);
   enemy.insertEnemy();
   var enemy2 = new Enemy(0, 325, board);
@@ -71,3 +71,58 @@ function Enemy(x, y, parent) {
 window.onload = function () {
   gameStart();
 };
+
+let lastShotTime = 0; //Flechas
+
+document.addEventListener("DOMContentLoaded", function() {
+  const player = document.getElementById('player');
+  const arrow = document.getElementById("arrow");
+
+  document.addEventListener("keydown", function(event) {
+    if (event.key === " " && canShoot()) {  
+      shootArrow();
+    }
+  });
+
+  function canShoot() {
+    const currentTime = Date.now();
+    const elapsedTimeSinceLastShot = currentTime - lastShotTime;
+    const timeToWait = 2000;  // Tiempo de espera entre flechas (2 segundos es lo óptimo)
+
+    if (elapsedTimeSinceLastShot >= timeToWait) {
+      lastShotTime = currentTime;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function shootArrow() {
+    const arrowTop = 345; 
+    const arrowLeft = 320; 
+    // Posición inicial de la flecha
+    arrow.style.top = `${arrowTop}px`;
+    arrow.style.left = `${arrowLeft}px`; 
+    arrow.style.display = "block";
+
+    // Calcula la posición final de la flecha en función de la rotación
+    const angleInRadians = rotation * (Math.PI / 180);
+    const deltaX = Math.sin(angleInRadians) * 321;
+    const deltaY = -Math.cos(angleInRadians) * 343;
+
+    // Animación de la flecha
+    const arrowAnimation = arrow.animate([
+      { top: `${arrowTop}px`, left: `${arrowLeft}px` },
+      { top: `${arrowTop + deltaY}px`, left: `${arrowLeft + deltaX}px` }
+    ], {
+      duration: 2000,
+      easing: "linear",
+      fill: "forwards"  
+    });
+
+
+    arrowAnimation.onfinish = function() {
+      arrow.style.display = "none";
+    };
+  }
+});
